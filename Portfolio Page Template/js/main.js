070ec7ba1872c3471af0480422411c2feb051764
ZedBear <zedbearcode@gmail.com>
2021-08-23 07:14:58 +1000

    test1

---
+++ Portfolio Page Template/js/main.js
@@ -0,0 +1,47 @@
+// console.log("Hello")
+
+new TypeIt('#element', {
+  strings: [
+    "Experienced Minecraft Developer",
+    "Experienced Community Manager",
+    "Java Engineer",
+    "Cool Guy!",
+  ],
+  breakLines: false,
+  loop: true
+})
+  
+  // Other callback methods include: 
+  // beforeString: (step, queue, instance) => {}
+  // afterStep: (step, queue, instance) => {}
+  // afterComplete: (instance) => {}
+
+// new TypeIt('#element', {
+//   loop: true,
+//   loopDelay: 45
+// })
+// .type('Service 1')
+// .pause(1000)
+// .options({speed: 200})
+// .delete(9)
+// .options({speed: 45})
+// .pause(300)
+// .type('Service 2')
+// .pause(1000)
+// .options({speed: 200})
+// .delete(9)
+// .options({speed: 45})
+// .pause(300)
+// .type('Service 3')
+// .pause(1000)
+// .options({speed: 200})
+// .delete(9)
+// .options({speed: 45})
+// .pause(300)
+// .type('Service 4')
+// .pause(1000)
+// .options({speed: 200})
+// .delete(9)
+// .options({speed: 45})
+// .pause(300);
+
